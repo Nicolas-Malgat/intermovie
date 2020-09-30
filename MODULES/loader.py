1e4cdd6f1bcb53f1e9fd34b94493f577092502d1
Audrey <audrey.cornaire@gmail.com>
2020-09-30 16:07:05 +0200

    [UPDATE] - Changement de la méthode 2 et correctifs

--- MODULES/loader.py
+++ MODULES/loader.py
@@ -1,4 +1,5 @@
-from os import path, listdir, getcwd
+import os
+from os import path, listdir, getcwd, makedirs
 
 import csv
 import requests
@@ -8,9 +9,13 @@ import zipfile
 import pandas as pd
 
 ZIP_REMOTE_PATH = 'https://stdatalake005.blob.core.windows.net/public/movies_dataset.zip'
-ZIP_LOCAL_PATH = '../datas/movies_dataset/movies_dataset.zip'
-RAW_LOCAL_PATH = '../datas/movies_dataset/'
-CURATED_LOCAL_PATH = '../datas/CURATED/'
+ZIP_LOCAL_PATH = 'DATAS/movies_dataset/movies_dataset.zip'
+RAW_LOCAL_PATH = 'DATAS/movies_dataset/'
+CURATED_LOCAL_PATH = 'DATAS/CURATED/'
+WORKS = 'WORKS/'
+FORMATS = 'FORMATS/'
+REGIONS = 'REGIONS/'
+GENRES = 'REGIONS/'
 
 
 class IntermovieDataLoader:
@@ -30,8 +35,15 @@ class IntermovieDataLoader:
             for row in file_stream_reader:
                 profession = row['category']
                 # Open a new file and write the header
+                if path.exists(CURATED_LOCAL_PATH + WORKS) == False:
+                    try:
+                        os.makedirs(CURATED_LOCAL_PATH + WORKS)
+                    except OSError:
+                        print ("Creation of the directory %s failed" % path)
+                        exit(1)
+
                 if profession not in open_files_references:
-                    output_file = open(CURATED_LOCAL_PATH + 'WORKS/{}.csv'.format(profession), 'w', encoding='utf-8', newline='')
+                    output_file = open(CURATED_LOCAL_PATH + WORKS +'{}.csv'.format(profession), 'w', encoding='utf-8', newline='')
                     dictionary_writer = csv.DictWriter(output_file, fieldnames=file_stream_reader.fieldnames)
                     dictionary_writer.writeheader()
                     open_files_references[profession] = output_file, dictionary_writer
@@ -56,8 +68,15 @@ class IntermovieDataLoader:
             for row in file_stream_reader:
                 titleType = row['titleType']
                 # Open a new file and write the header
+                if path.exists(CURATED_LOCAL_PATH + FORMATS) == False:
+                    try:
+                        os.makedirs(CURATED_LOCAL_PATH + FORMATS)
+                    except OSError:
+                        print ("Creation of the directory %s failed" % path)
+                        exit(1)
+
                 if titleType not in open_files_references:
-                    output_file = open(CURATED_LOCAL_PATH + 'FORMATS/{}.csv'.format(titleType), 'w', encoding='utf-8', newline='')
+                    output_file = open(CURATED_LOCAL_PATH + FORMATS + '{}.csv'.format(titleType), 'w', encoding='utf-8', newline='')
                     dictionary_writer = csv.DictWriter(output_file, fieldnames=file_stream_reader.fieldnames)
                     dictionary_writer.writeheader()
                     open_files_references[titleType] = output_file, dictionary_writer
@@ -82,8 +101,15 @@ class IntermovieDataLoader:
             for row in file_stream_reader:
                 region = row['region']
                 # Open a new file and write the header
+                if path.exists(CURATED_LOCAL_PATH + REGIONS) == False:
+                    try:
+                        os.makedirs(CURATED_LOCAL_PATH + REGIONS)
+                    except OSError:
+                        print ("Creation of the directory %s failed" % path)
+                        exit(1)
+
                 if region not in open_files_references:
-                    output_file = open(CURATED_LOCAL_PATH + 'REGIONS/{}.csv'.format(region), 'w', encoding='utf-8', newline='')
+                    output_file = open(CURATED_LOCAL_PATH + REGIONS + '{}.csv'.format(region), 'w', encoding='utf-8', newline='')
                     dictionary_writer = csv.DictWriter(output_file, fieldnames=file_stream_reader.fieldnames)
                     dictionary_writer.writeheader()
                     open_files_references[region] = output_file, dictionary_writer
@@ -93,10 +119,51 @@ class IntermovieDataLoader:
             for output_file, _ in open_files_references.values():
                 output_file.close()
 
+    def split_data_genres(self):
+        '''
+        Break raw data into many files
+        '''
+        
+        TITLE_FILE_NAME = 'title.basics.tsv'
+
+        with open(RAW_LOCAL_PATH + TITLE_FILE_NAME, encoding='utf-8') as file_stream:    
+            file_stream_reader = csv.DictReader(file_stream, delimiter='\t')
+            
+            open_files_references = {}
+
+            for row in file_stream_reader:
+                genre = row['genres']
+                # Open a new file and write the header
+                if path.exists(CURATED_LOCAL_PATH + GENRES) == False:
+                    try:
+                        os.makedirs(CURATED_LOCAL_PATH + GENRES)
+                    except OSError:
+                        print ("Creation of the directory %s failed" % path)
+                        exit(1)
+
+                if genre not in open_files_references:
+                    output_file = open(CURATED_LOCAL_PATH + GENRES +'{}.csv'.format(genre), 'w', encoding='utf-8', newline='')
+                    dictionary_writer = csv.DictWriter(output_file, fieldnames=file_stream_reader.fieldnames)
+                    dictionary_writer.writeheader()
+                    open_files_references[genre] = output_file, dictionary_writer
+                # Always write the row
+                open_files_references[genre][1].writerow(row)
+            # Close all the files
+            for output_file, _ in open_files_references.values():
+                output_file.close()
+
     def ensure_data_loaded(self):
         '''
         Ensure if data are already loaded. Download if missing
         '''
+        if path.exists(RAW_LOCAL_PATH) == False:
+            try:
+                os.makedirs(RAW_LOCAL_PATH)
+            except OSError:
+                print ("Creation of the directory %s failed" % path)
+                exit(1)
+            else:
+                print ("Successfully created the directory %s " % path)
 
         if path.exists(ZIP_LOCAL_PATH) == False:
             self._download_data()
@@ -144,9 +211,3 @@ class IntermovieDataLoader:
         with zipfile.ZipFile(ZIP_LOCAL_PATH, 'r') as zip_ref:
             zip_ref.extractall(RAW_LOCAL_PATH)
         print('Data extract successfully')
-
-
-if __name__ == "__main__" :
-
-    data_loader = IntermovieDataLoader()
-    data_loader.split_data_role()
